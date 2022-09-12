d0adc2a4b9637f692ecdc77f3a8f3af7bc3554cb
GathoniMbuthia <terrymbuthia02@gmail.com>
2022-09-12 05:12:02 -0700

    single digit combos

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,8 +8,14 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 99; num++)
-		putchar(num);
+	for (num = 0; num <= 9; num++)
+{
+	putchar((num % 10) + '0');
+	if (num == 0)
+		continue;
+	putchar(',');
+	putchar(' ');
+}
 	putchar('\n');
 	return (0);
 }
